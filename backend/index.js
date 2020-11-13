a39a0c281bc84d53fb30c7ec4c08c04ffb21425b
tiffany <lorytif@gmail.com>
2020-11-13 14:15:15 +0100

    Create Postgres and initialization backend

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
+const port = 3000
+
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+  })
+
+app.get('/planets', db.getAllPlanets)
+
+  app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+  })
+
+  
